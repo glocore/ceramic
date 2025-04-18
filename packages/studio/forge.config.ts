c5633ac1563debd27a39f5f8e00f05961ec2dfa6
Ashwin M <24620401+glocore@users.noreply.github.com>
2025-04-18 21:20:34 +0530

    add release workflow

--- packages/studio/forge.config.ts
+++ packages/studio/forge.config.ts
@@ -54,6 +54,18 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "glocore",
+          name: "ceramic",
+        },
+      },
+    },
+  ],
 };
 
 export default config;
